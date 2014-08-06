66de72a111428187b38ebf96970ee899bada5e43
Dale Salter <djsalter93@hotmail.com>
2014-08-06 20:44:35 +1000

    Initial Commit

---
+++ HashHable/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HashHable
+//
+//  Created by Dale Salter on 6/08/2014.
+//  Copyright (c) 2014 none. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
